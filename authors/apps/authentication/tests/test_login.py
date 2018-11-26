f870d6d74bbbb1c76793e59f002889b21fd7db25
Silas Kenneth <silas.kenneth@andela.com>
2018-11-26 16:22:09 +0300

    chore(test): add authentication tests
    - add login tests
    - add register tests
    - add base file for tests configuration
    - modify urls to run tests
    
    [Delivers #161966593]

---
+++ authors/apps/authentication/tests/test_login.py
@@ -0,0 +1,46 @@
+
+from rest_framework import status
+from authors.base_file import BaseTestCase
+
+
+class TestAuthentication(BaseTestCase):
+    """Test class for the authentication"""
+
+
+    def test_user_login(self):
+        """test login with accurate data"""
+        self.client.post(
+            self.register_url,
+            self.register_data,
+            format="json"
+        )
+        response = self.client.post(
+            self.login_url,
+            self.login_data,
+            format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_user_login_with_no_data(self):
+        """test login with invalid data"""
+        data = {
+                    "ksksk": "kskskk"
+                }
+        response = self.client.post(
+            self.login_url,
+            data,
+            format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_user_login_with_no_email(self):
+        """test login with no email"""
+
+        response = self.client.post(self.login_url, self.login_no_email, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        assert response.data['errors']["email"][0] == "This field may not be blank."
+
+    def test_user_login_with_invalid_password(self):
+        """login with invalid passwortd"""
+
+        response = self.client.post(self.login_url, self.login_invalid_pass, format='json')
+        self.assertEqual(response.status_code, 400)
+        assert response.data['errors']["error"][0] == "A user with this email and password was not found."
---
+++ authors/apps/authentication/tests/test_retrival.py
@@ -0,0 +1,30 @@
+"""File to test user retrival"""
+from rest_framework import status
+from authors.apps.authentication.models import User
+from authors.base_file import BaseTestCase
+
+
+class TestRetrieveUser(BaseTestCase):
+    """This class will test user"""
+
+    def test_get_user(self):
+        """test getting a user"""
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.get(self.user_url, self.login_data, format='json')
+        assert response.status_code == 200
+        assert response.data["email"] == "jake@jake.jake"
+        assert response.data["username"] == "jakejake"
+
+    def test_get_user_without_token(self):
+        """test getting user with no token"""
+        response = self.client.get(self.user_url, self.login_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        print(response.data["detail"])
+        assert response.data["detail"] == "Authentication credentials were not provided."
+
+    def test_get_user_invalid_or_expired_token(self):
+        """test getting user with wrong or expired token"""
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + "wrongtokenhere")
+        response = self.client.get(self.user_url, self.login_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        assert response.data["detail"] == "Invalid token"
