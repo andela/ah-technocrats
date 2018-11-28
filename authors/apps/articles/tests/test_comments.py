7ebbe3d494ee417c525fe9aec5f08f8b20232b2b
SnyderMbishai <csmbishai@gmail.com>
2018-11-28 18:08:40 +0300

    feat(testing): test getting a missing comment

--- authors/apps/articles/tests/test_comments.py
+++ authors/apps/articles/tests/test_comments.py
@@ -39,6 +39,14 @@ class TestComments(BaseTestCase):
         response2 = self.test_client.get(self.comment_url)
         self.asserEqual(response.status_code, status.HTTP_200_OK)
 
+    def test_getting_a_non_existing_comment(self):
+        """ Test getting a missing comment. """
+        self.user_signup()
+        self.user_login()
+        self.post_article()
+        response = self.test_client.get(self.comment_url)
+        self.asserEqual(response.status_code, status.HTTP_400_NOT_FOUND)
+
     def test_getting_all_comments(self):
         """ Test getting all comments to an article. """
         self.user_signup()
