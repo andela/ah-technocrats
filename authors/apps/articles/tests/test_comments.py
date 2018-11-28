e1ecc61bb7578f2df45379b85fa92e06a599d076
SnyderMbishai <csmbishai@gmail.com>
2018-11-28 17:11:31 +0300

    feat(testing): test successful comment posting

--- authors/apps/articles/tests/test_comments.py
+++ authors/apps/articles/tests/test_comments.py
@@ -1,10 +1,18 @@
-
+from rest_framework import status
 from .base_test import BaseTestCase
 
 class TestComments(BaseTestCase):
     """ Class for testing comments. """
-    pass
+    
     # test post comment
+    def test_comment_creation(self):
+        """ Test comment posting. """
+        self.user_signup()
+        self.user_login()
+        self.post_article()
+        response = self.post_comment()
+        self.asserEqual(response.status_code, status.HTTP_201_CREATED)
+    
     # test getting comment
     # test updating comment
     # test deleting comment
