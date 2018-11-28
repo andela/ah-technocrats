2859ce92c137caaf31a634a815edf2e5f9b9135e
SnyderMbishai <csmbishai@gmail.com>
2018-11-28 17:10:48 +0300

    feat(testing): add dummy data for testing

--- authors/apps/articles/tests/base_test.py
+++ authors/apps/articles/tests/base_test.py
@@ -13,15 +13,61 @@ class BaseTestCase(TestCase):
         #urls
         self.register_url = reverse("authentication:user-signup")
         self.login_url = reverse("authentication:user-login")
-        self.comments_url = reverse("articles")
+        self.articles_url = reverse("articles")
+        self.comments_url = reverse("articles:comments")
         
         self.register_data = {'user': {
-                                        "username": "nana",
-                                        "email": "nana@nana.nana",
-                                        "password": "nana123"
-                                    }}
+                                "username": "nana",
+                                "email": "nana@nana.nana",
+                                "password": "nana123"
+                            }}
 
         self.login_data = { "user": {
-                                        "email": "nana@nana.nana",
-                                        "password": "nana123"
-                                    }}
+                                "email": "nana@nana.nana",
+                                "password": "nana123"
+                            }}
+        
+        self.comment_data = { "comment": {
+                            "body": "His name was my name too."
+                        }}
+
+        self.article_data = { "article": {
+                            "title": "How to train your dragon",
+                            "description": "Ever wonder how?",
+                            "body": "You have to believe",
+                            "tagList": ["reactjs", "angularjs", "dragons"]
+                        }}
+    def user_signup(self):
+        """ Method for registering ba user for testing. """
+        res = self.client.post(
+        self.register_url,
+        self.register_data,
+        format="json")
+        return res
+    
+    
+    def user_login(self):
+        """ Method for logging in a user tor testing. """
+        res = self.client.post(
+        self.login_url,
+        self.login_data,
+        format='json')
+        return res
+
+    def post_article(self):
+        """ Post an article for testing. """
+        res = self.client.post(
+            self.articles_url,
+            self.article_data,
+            format='json'
+        )
+        return res
+
+    def post_comment(self):
+        """ Post comment to an article. """
+        res = self.client.post(
+                self.comments_url,
+                self.comment_data,
+                format='json'
+            )
+        return res
