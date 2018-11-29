9abdcdb9149fd3f26d4c501b42f55fd15e274f4b
salma-nyagaka <salmanyaga@gmail.com>
2018-11-29 21:58:31 +0300

    fix(documentation): reconfigure swagger documentation
    -change the url to include api/docs
    
    [Fixes #162313882]

--- authors/settings.py
+++ authors/settings.py
@@ -148,3 +148,16 @@ REST_FRAMEWORK = {
         # 'authors.apps.authentication.backends.JWTAuthentication',
     ),
 }
+
+SWAGGER_SETTINGS = {
+  'SHOW_REQUEST_HEADERS': True,
+  'USE_SESSION_AUTH': False,
+  'DOC_EXPANSION': 'list',
+  'SECURITY_DEFINITIONS': {
+      'api_key': {
+          'type': 'apiKey',
+          'in': 'header',
+          'name': 'Authorization'
+      }
+  }
+}
