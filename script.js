e619a936bafec020408cc9ed51a2ba9095e26b83
Silas Kenneth <silaskenneth1@gmail.com>
2018-12-20 11:26:46 +0300

    chore(update mockup): Add more functions
    - Navigation for bookmarks
    - Navigation for favorites
    - Bookmarks
    - Faovorites
    - Highlight comment
    - Comment edit history

--- script.js
+++ script.js
@@ -28,8 +28,14 @@ $('.ui.rating')
 ;
 try {
     var simplemde = new SimpleMDE();
+    simplemde.value("This text will appear in the editor");
 }catch (e) {
 
+}
+try {
+    $('.paragraph').quoteShare()
+}catch (e) {
+  console.log(e);
 }
 
 $('.ui.dropdown')
@@ -61,3 +67,20 @@ $('.ui.dropdown')
 $('.ui.modal')
   .modal('show')
 ;
+
+
+// Favorite Button - Heart
+$('.fav_me').click(function() {
+    $(this).toggleClass('active');
+});
+
+/* when a user clicks, toggle the 'is-animating' class */
+$(".fav_me").on('click touchstart', function(){
+    $(this).toggleClass('is_animating');
+});
+
+/*when the animation is over, remove the class*/
+$(".fav_me").on('animationend', function(){
+    $(this).toggleClass('is_animating');
+});
+
